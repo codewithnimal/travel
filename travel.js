fbe26ac2e499c7d491e18a88c097c7e7f1739c28
nimal <nimalvalakutti2006@gmail.com>
2026-02-16 22:48:19 +0530

    Initial commit

---
+++ travel.js
@@ -0,0 +1,15 @@
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+document.getElementById("contactForm").addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Message Sent Successfully!");
+});
